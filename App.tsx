974b25917037f95d767ebc9c5f9a1285b834ce68
Christoph Jerolimov <jerolimov+git+2018@gmail.com>
2018-05-09 21:57:12 +0200

    Add tslint configuration

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -19,7 +19,8 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
+interface Props {}
+
 export default class App extends Component<Props> {
   render() {
     return (
